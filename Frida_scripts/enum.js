0e450ed6492427ed509bc346c6bf87718f175dab
handoumeh <58666766+TK757567@users.noreply.github.com>
2023-10-11 12:49:26 +0300

    Delete Frida_scripts directory

--- Frida_scripts/enum.js
+++
@@ -1,5 +0,0 @@
-console.log("Enumerating methods of MainActivity");
-Java.perform(function() {
-  const groups = Java.enumerateMethods('*MainActivity*!*');
-  console.log(JSON.stringify(groups, null, 2));
-});
